39de2c2c8431787482d692793c478c90bef19144
Rafa0r7 <rafaelrodriguesrr06@gmail.com>
2025-12-04 10:02:25 -0300

    fix: alter created_at field to use auto_now_add for Note model

--- app/notes/models.py
+++ app/notes/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class Note(models.Model):
     title = models.CharField(_("Note"), max_length=255)
     content = models.TextField(_("Content"))
-    created_at = models.DateTimeField(_("Created_at"), auto_now=True)
+    created_at = models.DateTimeField(_("Created_at"), auto_now_add=True)
     user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
 
     def __str__(self):
